c030fe50f6ffcd73a7b3e86a3024cf4959211010
urmi-21 <mrbains21@gmail.com>
2021-03-25 14:08:42 -0500

    update

--- 3align_excercise.py
+++ 3align_excercise.py
@@ -1,6 +1,7 @@
 from pyrpipe.mapping import *
 
 
-star=Star(genome='data/refdata/',index='') # create a Star object to run STAR aligner
+star=Star(genome='data/refdata/',index='testout/testindex') # create a Star object to run STAR aligner
+
 
 
